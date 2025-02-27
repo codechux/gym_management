6049299504e44ffd904bfc8a86b75cd254d7394c
chux <chuxoguh@gmail.com>
2025-02-27 19:29:52 +0100

    testing

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -30,7 +30,7 @@ export function MainNav() {
     <div className="flex items-center space-x-4 lg:space-x-6">
       <Link href="/" className="flex items-center space-x-2">
         <Dumbbell className="h-6 w-6" />
-        <span className="font-bold text-xl hidden sm:inline-block">FitTrack</span>
+        <span className="font-bold text-xl hidden sm:inline-block">Fitzzz</span>
       </Link>
       <nav className="flex items-center space-x-4 lg:space-x-6">
         {routes.map((route) => (
